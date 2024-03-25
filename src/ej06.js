704d25acb1478e7d9b518a5e4b4494b0dfbffc46
MATIAS IVAN SZABO <47805194@ort>
2024-03-25 10:40:20 -0300

    falta el ultimo

--- src/ej06.js
+++ src/ej06.js
@@ -1,6 +1,13 @@
 let miUrl = null;
 let miObjeto = null;
 console.clear();
+miUrl = 'http://www.ort.edu.ar:8080/alumnos/index.htm?curso=2022&mes=mayo';
+miObjeto = parsearUrl(miUrl);
+console.log(miObjeto);
+
+miUrl = 'esto-no-es-una-url-valida';
+miObjeto = parsearUrl(miUrl);
+console.log(miObjeto);
 function parsearUrl(miUrl) {
     try {
         const parsedUrl = new URL(miUrl);
@@ -27,10 +34,4 @@ function parsearUrl(miUrl) {
     }
 }
 
-miUrl = 'http://www.ort.edu.ar:8080/alumnos/index.htm?curso=2022&mes=mayo';
-miObjeto = parsearUrl(miUrl);
-console.log(miObjeto);
 
-miUrl = 'esto-no-es-una-url-valida';
-miObjeto = parsearUrl(miUrl);
-console.log(miObjeto);
--- src/ej07.js
+++ src/ej07.js
@@ -1,9 +1,4 @@
-import pkg from 'currency-map-country';
-
-function obtenerMoneda(codigoPais) {
-    const pais = pkg.getCurrencyByCountryCode(codigoPais);
-    return pais ? pais.name : null;
-}
+import { getCurrency } from 'currency-map-country';
 
 let monedaDelPais, codigoPais;
 
@@ -15,3 +10,11 @@ console.log(`La moneda del país ${codigoPais} es: ${monedaDelPais}`);
 codigoPais = 'UZA';
 monedaDelPais = obtenerMoneda(codigoPais);
 console.log(`La moneda del país ${codigoPais} es: ${monedaDelPais}`);
+
+
+function obtenerMoneda(codigoPais) {
+    console.log('codigoPais', codigoPais);
+    const pais = getCurrency (codigoPais);
+    console.log('pais', pais);
+    return pais ? pais.name : null;
+}
